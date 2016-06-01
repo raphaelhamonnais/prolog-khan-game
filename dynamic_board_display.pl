c0a5b28889b9dad4011e9f6755389335ee7b862d
Louis Freret <Loukis95@gmail.com>
2016-06-01 15:39:43 +0200

    Affichage dynamique du board avec des couleurs. Chaque pion est un fait dynamique maintenant.

--- dynamic_board_display.pl
+++ dynamic_board_display.pl
@@ -1,7 +1,41 @@
+%% Base de faits dynamiques temporaires %%
 :-dynamic(i/1).
 :-dynamic(j/1).
 
+:-dynamic(s1j1/4).
+:-dynamic(s2j1/4).
+:-dynamic(s3j1/4).
+:-dynamic(s4j1/4).
+:-dynamic(s5j1/4).
+:-dynamic(kj1/4).
+:-dynamic(s1j2/4).
+:-dynamic(s2j2/4).
+:-dynamic(s3j2/4).
+:-dynamic(s4j2/4).
+:-dynamic(s5j2/4).
+:-dynamic(kj2/4).
+
+init() :- 	asserta(s1j1('S', 1, 0,0)),
+			asserta(s2j1('S', 1, 1,0)),
+			asserta(s3j1('S', 1, 2,0)),
+			asserta(s4j1('S', 1, 3,0)),
+			asserta(s5j1('S', 1, 4,0)),
+			asserta(kj1('K', 1, 5,0)),
+			asserta(s1j2('S', 2, 0,5)),
+			asserta(s2j2('S', 2, 1,5)),
+			asserta(s3j2('S', 2, 2,5)),
+			asserta(s4j2('S', 2, 3,5)),
+			asserta(s5j2('S', 2, 4,5)),
+			asserta(kj2('K', 2, 5,5)),
+			reset().
+
 reset():- retractall(i(_)), retractall(j(_)), asserta(i(0)), asserta(j(0)).
+
+writeWithPlayerColor(X, Player) :- 	Player = 1,
+									ansi_format([bold,fg(red)], '~w', [X]),!.
+writeWithPlayerColor(X, Player) :- 	Player = 2,
+									ansi_format([bold,fg(green)], '~w', [X]),!.
+writeWithPlayerColor(X, Player) :- 	ansi_format([bold,fg(white)], '~w', [X]),!.
 
 display([]).
 display([[T|Q1]|Q2]):- 	display([T|Q1]),
@@ -18,7 +52,7 @@ display([T|Q]):- 		i(I),
 						pawn(I, J, P, Player),
 						write(T),
 						write('-'),
-						write(P),
+						writeWithPlayerColor(P, Player),
 						write(' '),
 						NI is I+1,
 						asserta(i(NI)),
@@ -38,18 +72,3 @@ pawn(X, Y, P, J) :-	s4j2(P, J, X, Y), !.
 pawn(X, Y, P, J) :-	s5j2(P, J, X, Y), !.
 pawn(X, Y, P, J) :-	kj2(P, J, X, Y), !.
 pawn(_, _, '_', 0).
-
-%% Base de faits statiques temporaires %%
-
-s1j1('S', 1, 0,0).
-s2j1('S', 1, 1,0).
-s3j1('S', 1, 2,0).
-s4j1('S', 1, 3,0).
-s5j1('S', 1, 4,0).
-kj1('K', 1, 5,0).
-s1j2('S', 2, 0,5).
-s2j2('S', 2, 1,5).
-s3j2('S', 2, 2,5).
-s4j2('S', 2, 3,5).
-s5j2('S', 2, 4,5).
-kj2('K', 2, 5,5).
