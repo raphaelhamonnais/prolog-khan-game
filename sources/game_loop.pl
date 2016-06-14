4bc1c55ccf2c66da73ca4c5be3d2fa4a6e4a9f45
raphaelhamonnais <raphael.hamonnais@gmail.com>
2016-06-14 18:06:39 +0200

    travail sur le rapport

--- sources/game_loop.pl
+++ sources/game_loop.pl
@@ -3,6 +3,12 @@
 
 
 
+player_win(Player) :-
+		get_other_player(Player,OtherPlayer),
+		get_unused_player_pawns(OtherPlayer, UnusedPawnList),
+		member('K',UnusedPawnList),!.
+
+
 human_vs_human_game_loop(Player) :-
 		ask_movement_to_player(Player),
 		get_other_player(Player, OtherPlayer),
@@ -19,12 +25,6 @@ human_vs_human_launch_game() :-
 		player1(Player_1),player2(Player_2), % avoir les valeurs numériques correspondantes au joueur 1 et 2
 		human_vs_human_game_loop(Player_1).
 
-
-
-player_win(Player) :-
-		get_other_player(Player,OtherPlayer),
-		get_unused_player_pawns(OtherPlayer, UnusedPawnList),
-		member('K',UnusedPawnList),!.
 
 
 
