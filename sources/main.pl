bc7b96f0cecc799ce1ded5ac9369211731c6b4bd
raphaelhamonnais <raphael.hamonnais@gmail.com>
2016-06-16 11:03:30 +0200

    ...

--- sources/main.pl
+++ sources/main.pl
@@ -15,8 +15,8 @@ main() :-
 	include_sources(), % inclure tous les fichiers
 	reset_all_dynamic_facts(), % supprimer tous les faits dynamiques
 	initBoard(), % afficher le plateau initial avec choix de la disposition
-	%ask_initial_pawns_placement(), % demander aux joueurs de placer leurs pions
-	init_test_board(),
+	ask_initial_pawns_placement(), % demander aux joueurs de placer leurs pions
+	%init_test_board(),
 	launch_game().
 
 
